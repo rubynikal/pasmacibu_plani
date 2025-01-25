0a5639aed4d6f4fdab491ffb42f33c8fd55b6151
rubynikal <nika.zaharova06@gmail.com>
2025-01-25 17:26:51 +0200

    Update jauns_plans.py

--- blueprints/jauns_plans.py
+++ blueprints/jauns_plans.py
@@ -6,7 +6,7 @@ import regex
 import datetime
 
 #gemini api iestatījumi
-os.environ["API_KEY"] = 'AIzaSyBgn8YiRnasLmPpQoDhc7TEcJpqO7LACuM'
+os.environ["API_KEY"] = 'API atslēga'
 genai.configure(api_key=os.environ["API_KEY"])
 
 model_config = {
